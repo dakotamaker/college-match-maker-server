9779063d53605a16be280d6ff1f534eac366a6d5
Dakota Maker <dakotamaker@gmail.com>
2020-07-12 16:03:06 -0500

    Set header in the correct router

--- routes/query.js
+++ routes/query.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const query = require('./db/index').query
 
 router.post('/', async function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
   let vals = await query(req.body.sql || '', req.body.sqlParams || []);
   res.json(vals);
 });
